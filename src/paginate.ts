b0ec4254712ab5b63c595d64881adc46733c0cad
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-27 14:38:08 -0600

    Update paginate.ts
    Delete

--- src/paginate.ts
+++ src/paginate.ts
@@ -1,22 +1,2 @@
-import type { Octokit } from "@octokit/core";
-import { mergeResponses } from "./merge-responses.js";
-import { createIterator } from "./iterator.js";
+delete
 
-const createPaginate = (octokit: Octokit) => {
-  const iterator = createIterator(octokit);
-  return async <ResponseType extends object = any>(
-    query: string,
-    initialParameters: Record<string, any> = {},
-  ): Promise<ResponseType> => {
-    let mergedResponse: ResponseType = {} as ResponseType;
-    for await (const response of iterator<ResponseType>(
-      query,
-      initialParameters,
-    )) {
-      mergedResponse = mergeResponses<ResponseType>(mergedResponse, response);
-    }
-    return mergedResponse;
-  };
-};
-
-export { createPaginate };
